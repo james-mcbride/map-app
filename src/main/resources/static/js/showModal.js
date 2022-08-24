2d861d39f853d22a114a55cadab3e96ca8989f6d
james McBride <james.m.mcbride15@gmail.com>
2022-08-23 20:12:44 -0500

    removed static files

--- src/main/resources/static/js/showModal.js
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-const myModal = document.getElementById('myModal');
-const myInput = document.getElementById('myInput');
-
-myModal.addEventListener('shown.bs.modal', function () {
-    myInput.focus()
-})
